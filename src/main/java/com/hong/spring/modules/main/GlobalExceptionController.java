d7ff77441da91e223c28705ce3387f2cc9699d47
allbegray <allbegray@nate.com>
2015-02-23 13:24:07 +0900

    404 에러 핸들러 추가

--- src/main/java/com/hong/spring/modules/main/GlobalExceptionController.java
+++ src/main/java/com/hong/spring/modules/main/GlobalExceptionController.java
@@ -2,10 +2,13 @@ package com.hong.spring.modules.main;
 
 import java.sql.SQLException;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 @ControllerAdvice
 public class GlobalExceptionController {
 
@@ -19,6 +22,13 @@ public class GlobalExceptionController {
 	@ExceptionHandler(Exception.class)
 	public ModelAndView handleAllException(Exception ex) {
 		ModelAndView mav = new ModelAndView("/error/generic_error");
+		mav.addObject("exception", ex);
+		return mav;
+	}
+
+	@RequestMapping("/error/404")
+	public ModelAndView handle404(Exception ex) {
+		ModelAndView mav = new ModelAndView("/error/404");
 		mav.addObject("exception", ex);
 		return mav;
 	}
