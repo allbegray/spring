36fdbfbba109ce0a537c79eb9a95279ffa27e6fa
allbegray <allbegray@nate.com>
2015-02-24 17:13:52 +0900

    세션 타임 아웃 필터 bean 제거

--- src/main/java/com/hong/spring/common/config/SecurityConfig.java
+++ src/main/java/com/hong/spring/common/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.hong.spring.common.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,12 +23,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		super.configure(http);
 		
-		http.addFilterAfter(ajaxSessionTimeoutFilter(), ExceptionTranslationFilter.class);
-	}
-
-	@Bean
-	public AjaxSessionTimeoutFilter ajaxSessionTimeoutFilter() {
-		return new AjaxSessionTimeoutFilter();
+		http.addFilterAfter(new AjaxSessionTimeoutFilter(), ExceptionTranslationFilter.class);
 	}
 
 }
