148135a67a8d03b0347346eb3f268f7fbd88a2f7
allbegray <allbegray@nate.com>
2015-02-27 18:23:27 +0900

    bower_components_minify_copy 테스크 수정

--- gulpfile.js
+++ gulpfile.js
@@ -10,27 +10,34 @@ gulp.task('clean', function(cb) {
 
 gulp.task('bower_components_minify_copy', ['clean'], function () {
 	
-	gulp.src('bower_components/font-awesome/fonts/*.*').pipe(gulp.dest(DIST_PATH + '/font-awesome/fonts/'));
-	gulp.src('bower_components/font-awesome/css/font-awesome.min.css').pipe(gulp.dest(DIST_PATH + '/font-awesome/css/'));
+	var mappings = {
+		'bower_components/font-awesome/fonts/*.*' : '/font-awesome/fonts/',
+		'bower_components/font-awesome/css/font-awesome.min.css' : '/font-awesome/css/',
+		
+		'bower_components/jquery/dist/jquery.min.js' : '/jquery/',
+		
+		'bower_components/jqgrid/js/minified/jquery.jqGrid.min.js' : '/jqgrid/',
+		'bower_components/jqgrid/js/i18n/grid.locale-en.js' : '/jqgrid/js/i18n/',
+		'bower_components/jqgrid/js/i18n/grid.locale-kr.js' : '/jqgrid/js/i18n/',
+		'bower_components/jqgrid/css/ui.jqgrid.css' : '/jqgrid/css',
+		
+		'bower_components/jquery-ui/jquery-ui.min.js' : '/jquery-ui/',
+		'bower_components/jquery-ui/themes/redmond/**/*' : '/jquery-ui/themes/redmond/',
+		
+		'bower_components/moment/min/moment-with-locales.min.js' : '/moment/',
+		
+		'bower_components/jgrowl/jquery.jgrowl.min.js' : '/jgrowl/',
+		'bower_components/jgrowl/jquery.jgrowl.min.css' : '/jgrowl/',
+		
+		'bower_components/handlebars/handlebars.min.js' : '/handlebars/',
+		
+		'bower_components/angular/angular.min.js' : '/angular/'
+	};
 	
-	gulp.src('bower_components/jquery/dist/jquery.min.js').pipe(gulp.dest(DIST_PATH + '/jquery/'));
-	
-	gulp.src('bower_components/jqgrid/js/minified/jquery.jqGrid.min.js').pipe(gulp.dest(DIST_PATH + '/jqgrid/'));
-	gulp.src('bower_components/jqgrid/js/i18n/grid.locale-en.js').pipe(gulp.dest(DIST_PATH + '/jqgrid/js/i18n/'));
-	gulp.src('bower_components/jqgrid/js/i18n/grid.locale-kr.js').pipe(gulp.dest(DIST_PATH + '/jqgrid/js/i18n/'));
-	gulp.src('bower_components/jqgrid/css/ui.jqgrid.css').pipe(gulp.dest(DIST_PATH + '/jqgrid/css/'));
-	
-	gulp.src('bower_components/jquery-ui/jquery-ui.min.js').pipe(gulp.dest(DIST_PATH + '/jquery-ui/'));
-	gulp.src('bower_components/jquery-ui/themes/redmond/**/*').pipe(gulp.dest(DIST_PATH + '/jquery-ui/themes/redmond/'));
-	
-	gulp.src('bower_components/moment/min/moment-with-locales.min.js').pipe(gulp.dest(DIST_PATH + '/moment/'));
-	
-	gulp.src('bower_components/jgrowl/jquery.jgrowl.min.js').pipe(gulp.dest(DIST_PATH + '/jgrowl/'));
-	gulp.src('bower_components/jgrowl/jquery.jgrowl.min.css').pipe(gulp.dest(DIST_PATH + '/jgrowl/'));
-	
-	gulp.src('bower_components/handlebars/handlebars.min.js').pipe(gulp.dest(DIST_PATH + '/handlebars/'));
-	
-	gulp.src('bower_components/angular/angular.min.js').pipe(gulp.dest(DIST_PATH + '/angular/'));
+	for (var k in mappings) {
+		gulp.src(k).pipe(gulp.dest(DIST_PATH + mappings[k]));
+		console.log('copy ' + k + ' -> ' + (DIST_PATH + mappings[k]));
+	}
 	
 });
 
